1328b5abdc51b3f3e256a2471ea43d0681c4b938
mahdavipanah <h.mahdavipanah@gmail.com>
2016-12-24 12:07:49 +0330

    Add best_first_search algorithm

---
+++ algorithms/util/best_first_seach.py
@@ -0,0 +1,34 @@
+"""
+pynpuzzle - Solve n-puzzle with Python
+
+Best-first search algorithm
+
+Version : 1.0.0
+Author : Hamidreza Mahdavipanah
+Repository: http://github.com/mahdavipanah/pynpuzzle
+License : MIT License
+"""
+import heapq
+from .tree_search import Node
+
+def search(state, goal_state, fn):
+    """Best-first search"""
+    queue = []
+    entrance = 0
+    node = Node(state)
+    while not node.is_goal(goal_state):
+        node.expand()
+        for child in node.children:
+            queue_item = (fn(child), entrance, child)
+            heapq.heappush(queue, queue_item)
+            entrance += 1
+        node = heapq.heappop(queue)[2]
+
+    output = []
+    output.append(node.state)
+    for parent in node.parents():
+        output.append(parent.state)
+    output.reverse()
+
+    return output
+
