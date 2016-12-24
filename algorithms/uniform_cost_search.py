231d7fd7074f55f715a1af5432cd6c87813cd97c
mahdavipanah <h.mahdavipanah@gmail.com>
2016-12-24 12:08:10 +0330

    Add uniform-cost search algorithm

---
+++ algorithms/uniform_cost_search.py
@@ -0,0 +1,19 @@
+"""
+pynpuzzle - Solve n-puzzle with Python
+
+Uniform-cost search algorithm
+
+Version : 1.0.0
+Author : Hamidreza Mahdavipanah
+Repository: http://github.com/mahdavipanah/pynpuzzle
+License : MIT License
+"""
+from .util import best_first_seach as bfs
+
+
+def search(state, goal_state):
+    """Uniform-cost search"""
+    def gn(node):
+        return node.gn()
+
+    return bfs.search(state, goal_state, gn)
